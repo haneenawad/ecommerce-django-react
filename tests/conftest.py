a9dde378b6e7fe850d81415f7e567659b200b0f1
“Haneen <haneen.awad1996@gmail.com>
2022-07-15 16:12:02 +0300

    added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,16 @@
-import pytest
-from rest_framework.test import APIClient
-from django.db import models
-from django.contrib.auth.models import User
-
-
-@pytest.fixture
-def user():
-    return User.objects.create_user(username="Haneen",
-                                    email="haneen@gmail.com",
-                                    password="h12345678")
-
-
-@pytest.fixture
-def client():
-    return APIClient
+# import pytest
+# from rest_framework.test import APIClient
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+#
+# @pytest.fixture
+# def user():
+#     return User.objects.create_user(username="Haneen",
+#                                     email="haneen@gmail.com",
+#                                     password="h12345678")
+#
+#
+# @pytest.fixture
+# def client():
+#     return APIClient
